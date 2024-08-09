699b6ed2401b8c24695635af9929cbf37d730ff9
Decade-qiu <3237550600@qq.com>
2024-08-09 10:28:30 +0800

    异步协程爬虫

--- 爬虫课件/第六章：高性能异步爬虫/7.aiohttp实现多任务异步协程.py
+++ 爬虫课件/第六章：高性能异步爬虫/7.aiohttp实现多任务异步协程.py
@@ -1,48 +1,26 @@
-#环境安装：pip install aiohttp
-#使用该模块中的ClientSession
-import requests
 import asyncio
 import time
 import aiohttp
 
 start = time.time()
-# urls = [
-#     'http://127.0.0.1:5000/bobo','http://127.0.0.1:5000/jay','http://127.0.0.1:5000/tom',
-#     'http://127.0.0.1:5000/bobo', 'http://127.0.0.1:5000/jay', 'http://127.0.0.1:5000/tom',
-#     'http://127.0.0.1:5000/bobo', 'http://127.0.0.1:5000/jay', 'http://127.0.0.1:5000/tom',
-#     'http://127.0.0.1:5000/bobo', 'http://127.0.0.1:5000/jay', 'http://127.0.0.1:5000/tom',
-#
-# ]
-from multiprocessing.dummy import Pool
-pool = Pool(2)
 
-urls = []
-for i in range(10):
-    urls.append('http://127.0.0.1:5000/bobo')
-print(urls)
+urls = ['http://127.0.0.1:5000/bobo' for _ in range(10)]
+
 async def get_page(url):
     async with aiohttp.ClientSession() as session:
-        #get()、post():
-        #headers,params/data,proxy='http://ip:port'
-        async with await session.get(url) as response:
-            #text()返回字符串形式的响应数据
-            #read()返回的二进制形式的响应数据
-            #json()返回的就是json对象
-            #注意：获取响应数据操作之前一定要使用await进行手动挂起
+        async with session.get(url) as response:
             page_text = await response.text()
             print(page_text)
 
-tasks = []
+async def main():
+    tasks = [asyncio.create_task(get_page(url)) for url in urls]
+    await asyncio.wait(tasks)
 
-for url in urls:
-    c = get_page(url)
-    task = asyncio.ensure_future(c)
-    tasks.append(task)
-
-loop = asyncio.get_event_loop()
-
-loop.run_until_complete(asyncio.wait(tasks))
+# 使用 asyncio.run() 来运行主协程
+asyncio.run(main())
+# 串行请求
+# for url in urls:
+#     asyncio.run(get_page(url))
 
 end = time.time()
-
-print('总耗时:',end-start)
+print('总耗时:', end-start)
